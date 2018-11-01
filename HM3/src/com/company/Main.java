ee0abe41befaa6e1d6f32ba21cfcc4b8c7f124d2
Mihai Alexandru Manolescu <alex.inntekt@gmail.com>
2018-11-01 18:30:00 +0200

    finished HM3 i think

--- HM3/src/com/company/Main.java
+++ HM3/src/com/company/Main.java
@@ -8,16 +8,7 @@ public class Main
 
     public static void main(String[] args)
     {
-//	   Deck myDeck = new Deck();
-//	   Deck one = new Deck();
-//	   Deck two = new Deck();
 
-
-
-
-	   //myDeck.createFreshPack();
-	   //myDeck.shuffle();
-	   //myDeck.display();
 
         GameController game = new GameController();
         game.prepareGame();
