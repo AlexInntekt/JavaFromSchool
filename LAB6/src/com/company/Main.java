09401fea13f3c463bdb76fbe1794603e05a42601
Mihai Alexandru Manolescu <alex.inntekt@gmail.com>
2018-11-02 13:09:13 +0200

    f

--- LAB6/src/com/company/Main.java
+++ LAB6/src/com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.awt.*;
 import java.io.*;
-import java.util.List;
+
 
 
 
@@ -44,7 +44,7 @@ public class Main
     {
         super("Add new book");
         setSize(400,600);
-        setLayout(new GridLayout(8,4));
+        setLayout(new GridLayout(12,2));
 
         add(labelOne);
         add(textFieldOne);
@@ -55,24 +55,19 @@ public class Main
 
         add(new Checkbox("yes/no", null, true));
 
-//
-//        text = new TextField(20);
-//        text.setText("introdu date");
-//
-//        add(text);
-//
-//        button1 = new Button ("Save");
-//        add(button1);
-//
-//        button1.setLabel("Save as");
-//        button2 = new Button ("Cancel");
-//        add(button2);
-//
-//        tArea=new TextArea(10,20);
-//        add(tArea);
-//        tArea.setText("Hi");
-//        tArea.append("John");
-//        tArea.insert(" ",2);
+        add(new Label("Type of book: "));
+        List list1 = new List(1, false);
+        list1.addItem("Beletristica");
+        list1.addItem("Psihologie");
+        list1.addItem("Dezvoltare personala");
+        add(list1);
+
+//        Canvas c=new Canvas();
+////        c.resize(40,40);
+////        c.setBackground(Color.black);
+////        add(c);
+
+
     }
 }
 
