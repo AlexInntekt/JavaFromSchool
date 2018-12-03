18c6e4f70aaa085363266a9716171f389d649fd0
Mihai Alexandru Manolescu <alex.inntekt@gmail.com>
2018-12-03 02:03:56 +0200

    fixed a bug

--- HM threads/src/com/company/Main.java
+++ HM threads/src/com/company/Main.java
@@ -6,10 +6,11 @@ public class Main {
     {
         System.out.println("Method main() runs in thread " +
                 Thread.currentThread().getName()); // (5) Current thread
-        Counter counterA = new Counter("1", 10); // (6) Create a thread.
-        Counter counterB = new Counter("2", 20); // (7) Create a thread.
-        Counter counterC = new Counter("3", 34); // (7) Create a thread.
-        Counter counterCD= new Counter("4", 17); // (7) Create a thread.
+        Counter one = new Counter("1", 10); // (6) Create a thread.
+        Counter two = new Counter("2", 20); // (7) Create a thread.
+        Counter three = new Counter("3", 34); // (7) Create a thread.
+        Counter four= new Counter("4", 17); // (7) Create a thread.
+        Counter five= new Counter("5", 5); // (7) Create a thread.
         System.out.println("Exit from main() method.");
     }
 }
@@ -32,8 +33,8 @@ class Counter extends Thread {
 
     public void run() {
         try {
-            while (currentValue < 50) {
-                System.out.println("Thread id "+getName()+" is at step "+(currentValue++)+"/"+this.totalSteps);
+            while (currentValue < this.totalSteps) {
+                System.out.println("Thread id "+getName()+" is at step "+(currentValue++)+" / "+this.totalSteps);
                 Thread.sleep(50); // (4) Current thread sleeps.
             }
         } catch (InterruptedException e) {
