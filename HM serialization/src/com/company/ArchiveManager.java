9651f9538527e5c4be29a4cbfdc8b7295e8b70a2
Mihai Alexandru Manolescu <alex.inntekt@gmail.com>
2018-12-14 09:44:29 +0200

    good job. it works.

---
+++ HM serialization/src/com/company/ArchiveManager.java
@@ -0,0 +1,109 @@
+package com.company;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class ArchiveManager
+{
+    public static void write(String setName, String setSurname, int setSSN, int setAge, String setGender, String setCity, String setCountry, String setAdress)
+    {
+        //constructor:
+        //(setName, setSurname, setSSN, setAge, setGender, setCity, setCountry)
+        Person one = new Person(setName,setSurname,setSSN,setAge,setGender,setCity,setCountry, setAdress);
+
+
+        ArrayList<Person> archive=new ArrayList<>();
+                          archive = read();
+
+        archive.add(one);
+
+
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("/users/alex/desktop/DataStreamLab/employee.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(archive);
+
+
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in /users/alex/desktop/DataStreamLab/employee.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Person> read()
+    {
+        ArrayList<Person> list=new ArrayList<>();
+
+
+        try {
+            FileInputStream fileIn = new FileInputStream("/users/alex/desktop/DataStreamLab/employee.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            list = (ArrayList<Person>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+            return null;
+        }
+
+
+
+
+        System.out.print("\n\n");
+
+        for(int i=0;i<list.size();i++)
+        {
+            Person current = list.get(i);
+            System.out.println("Deserialized Employee...");
+            System.out.println("Name: " + current.name + " " + current.surname);
+            System.out.println("Gender: " + current.gender);
+            System.out.println("Age: " + current.age);
+            System.out.println("City: " + current.city);
+            System.out.println("Country: " + current.country);
+            System.out.println("Adress: " + current.adress);
+            System.out.println("SSN: " + current.SSN);
+            System.out.println("\n\n\n___________________\n");
+        }
+
+        return list;
+    }
+
+
+    public ArchiveManager()
+    {
+        //constructor:
+        //(setName, setSurname, setSSN, setAge, setGender, setCity, setCountry)
+        Person one = new Person("Keli","Malinghton",124,22,"F","Thompson","UK", "Ave Etilka 23");
+        Person two = new Person("Richard","Mollen",331,31,"M","Thompson","USA", "str Roses, 91");
+        Person three = new Person("Vasile","Dumitrescu",273,29,"M","Thompson","Romania", "str Lorry, 23 bis");
+
+
+        ArrayList<Person> archive=new ArrayList<>();
+
+        archive.add(one);
+        archive.add(two);
+        archive.add(three);
+
+
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("/users/alex/desktop/DataStreamLab/employee.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(archive);
+
+
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in /users/alex/desktop/DataStreamLab/employee.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+}
--- HM serialization/src/com/company/menuUI.java
+++ HM serialization/src/com/company/menuUI.java
@@ -1,25 +1,33 @@
 package com.company;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class menuUI extends Frame
 {
-    Label labelOne = new Label("Product ");
-    TextField textFieldOne=new TextField("1",20);
+    Label labelOne = new Label("Name ");
+    TextField textFieldOne=new TextField("Ionel",20);
 
-    Label labelTwo = new Label("Quantity ");
-    TextField textFieldTwo=new TextField("2",20);
+    Label labelTwo = new Label("Surname ");
+    TextField textFieldTwo=new TextField("Petrescu",20);
 
-    Label output = new Label("Total ");
-    TextField textFieldThree=new TextField(" ",20);
+    Label output = new Label("City ");
+    TextField textFieldThree=new TextField("Bucharest",20);
+
+    private Button insert;
+    private Button show;
+
+    Panel p=new Panel();
+    Panel p2=new Panel();
 
     public menuUI()
     {
         super("Insert a person in the DB");
 
-        setSize(400,400);
+        setSize(400,750);
         //setLayout(new GridLayout(12,12));
-        setLayout(new GridLayout(6,24));
+        setLayout(new GridLayout(12,12));
 
         add(labelOne);
         add(textFieldOne);
@@ -27,6 +35,49 @@ public class menuUI extends Frame
         add(textFieldTwo);
         add(output);
         add(textFieldThree);
+
+        insert = new Button ("Insert a new person");
+        show = new Button("Show DB");
+        add(insert);
+        add(show);
+
+        insert.addActionListener(new ActionListener()
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.print("\n Serialize object 'person' \n");
+
+                ArchiveManager.write("alex","manolescu",23,22,"M","Furth","Romania","1 mai");
+//                System.out.println("result");
+//
+//
+//
+//                currentResult=Double.valueOf(textFieldOne.getText())*Double.valueOf(textFieldTwo.getText());
+//
+//                textFieldThree.setText(String.valueOf(currentResult));
+
+            }
+        });
+
+        show.addActionListener(new ActionListener()
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.print("\n Displaying objects. Deserialize\n");
+
+                  ArchiveManager.read();
+//                System.out.println("result");
+//
+//
+//
+//                currentResult=Double.valueOf(textFieldOne.getText())*Double.valueOf(textFieldTwo.getText());
+//
+//                textFieldThree.setText(String.valueOf(currentResult));
+
+            }
+        });
     }
 
 }
